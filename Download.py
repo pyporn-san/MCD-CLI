cd3cfbe8f33669b8878d9a10d191cd6779b37929
pyporn-san <76444728+pyporn-san@users.noreply.github.com>
2020-12-26 17:47:17 +0330

    initial commit

---
+++ Download.py
@@ -0,0 +1,102 @@
+import concurrent.futures
+import datetime
+import os
+from itertools import repeat
+
+from colorama import Fore, init
+from multporn import Multporn, Utils, Webpage
+from termcolor import colored
+
+init()
+
+
+def main():
+    # Choosing the links' input method
+    try:
+        print("Mode :")
+        print(f"{colored('0', 'green')} To load from \"links.txt\" {colored('default', 'cyan')}")
+        print(f"{colored('1', 'green')} For finding links in a webpage")
+        print(f"{colored('2', 'green')} To search a query")
+        Mode = int(cinput(f"{colored('3', 'green')} To input links manually "))
+        if(Mode > 3 or Mode < 0):
+            Mode = 0
+    except ValueError:
+        Mode = 0
+    links = provideLinks(Mode)
+    # Calling the Download function
+    start = datetime.datetime.now()
+    if links:
+        with concurrent.futures.ThreadPoolExecutor(max_workers=len(links)) as executor:
+            executor.map(Multporn, links, repeat(True))
+    print(f"Done, took ", colored(datetime.datetime.now()-start, 'yellow'))
+    _continue = cinput(f"Continue {colored('[Y/(N)]', 'cyan')}")
+    if (_continue.lower().startswith("y")):
+        return True
+    else:
+        return False
+
+
+def provideLinks(arg):
+    switcher = {
+        0: fromFile,
+        1: fromWebpage,
+        2: search,
+        3: manual}
+    return switcher.get(arg)()
+
+
+def fromFile():
+    try:
+        with open("links.txt", "r") as f:
+            links = f.read().splitlines()
+            if(not links):
+                print(colored('\"links.txt\" is empty', 'red'))
+            return links
+    except:
+        print(colored('\"links.exe\" doesn\'t exist', 'red'))
+        with open("links.txt", "w") as _:
+            pass
+        links = []
+
+
+def fromWebpage():
+    page = Webpage(input("The webpage to load links from : "))
+    return page.links
+
+
+def search():
+    query = input("Search query: ")
+    try:
+        page = int(
+            cinput(f"What pages to load {colored('(Defaults to first page)', 'cyan')}: "))
+    except:
+        page = 1
+    links = Utils.Search(query, page)
+    for i in range(len(links)):
+        print(f"{colored(f'{i+1}.', 'green')} {links[i]}")
+    try:
+        page = int(
+            cinput(f"Choose comic {colored('(Defaults to first)', 'cyan')}: "))-1
+    except ValueError:
+        page = 0
+    return [links[page]]
+
+
+def manual():
+    try:
+        num = int(cinput(f"Number of links {colored('1', 'cyan')}: "))
+    except ValueError:
+        num = 1
+    links = [input(f"Link {str(i + 1)} : ") for i in range(num)]
+    return links
+
+
+def cinput(prompt):
+    print(prompt, end="")
+    return input()
+
+
+if __name__ == "__main__":
+    # Main loop
+    while main():
+        pass
---
+++ README.md
@@ -0,0 +1,7 @@
+# Multporn-cli
+
+A CLI using [Multporn](https://github.com/pyporn-san/Multporn)
+
+## Usage
+
+Just download the source and run Download.py
